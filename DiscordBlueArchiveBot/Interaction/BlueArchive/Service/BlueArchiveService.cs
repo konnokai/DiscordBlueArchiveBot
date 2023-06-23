f06f60a37c03a77fc87e11e1185696d400946dcf
Konnokai <f20001012@gmail.com>
2023-06-23 11:16:22 +0800

    add: 排除泳裝垃圾桶

--- DiscordBlueArchiveBot/Interaction/BlueArchive/Service/BlueArchiveService.cs
+++ DiscordBlueArchiveBot/Interaction/BlueArchive/Service/BlueArchiveService.cs
@@ -19,7 +19,7 @@ namespace DiscordBlueArchiveBot.Interaction.BlueArchive.Service
         public bool IsRefreshingData { get; private set; } = true;
 
         // https://kamigame.jp/bluearchive/page/240666421468632572.html#%E9%85%8D%E5%B8%83%E3%82%AD%E3%83%A3%E3%83%A9%E4%B8%80%E8%A6%A7
-        public ushort[] EventStudents { get; private set; } = new ushort[] { 13004, 26006, 16006, 26008, 16008, 16009, 26007, 20004, 16010, 16011, 16007, 16012, 26009 };
+        public ushort[] EventStudents { get; private set; } = new ushort[] { 13004, 26006, 16006, 26008, 16008, 16009, 26007, 20004, 16010, 16011, 16007, 16012, 26009, 26010 };
         public List<Student> Students => _students?.Data;
         public List<Student> JPPickUpDatas { get; private set; } = new();
         public List<Student> GlobalPickUpDatas { get; private set; } = new();
