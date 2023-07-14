b19c2d190f22970dd171515d004ad643f3ae6656
孤之界 <f20001012@gmail.com>
2023-07-14 13:37:49 +0800

    add: 初始化 Migrations

---
+++ DiscordBlueArchiveBot/Migrations/20230714053730_Init.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscordBlueArchiveBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CafeInviteTicketUpdateTime",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    RegionTypeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    NotifyDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DateAdded = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CafeInviteTicketUpdateTime", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NotifyConfig",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    NotifyTypeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    RegionTypeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DateAdded = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotifyConfig", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CafeInviteTicketUpdateTime");
+
+            migrationBuilder.DropTable(
+                name: "NotifyConfig");
+        }
+    }
+}
